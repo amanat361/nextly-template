539af679d5493f04748a2dd07a720b20472c0471
Samin Amanat <amanat361@gmail.com>
2023-08-26 17:45:35 -0700

    fixed project page layout

--- components/work.js
+++ components/work.js
@@ -2,19 +2,24 @@ import Image from "next/image";
 
 export default function Work({item}) {
     return (
-        <article className='grid grid-cols-1 sm:grid-cols-3 border-b-2 py-6'>
-            <div className='p-4'>
-                <Image src={item.image} className='rounded-lg' width={500} height={335} />
-            </div>
-            <div className="col-span-2">
+        <article className='grid grid-cols-1 sm:grid-cols-2 border-b-2 py-6 gap-4'>
+            <Image src={item.image} width={500} height={500} className="object-cover rounded-lg object-top w-full max-h-80" />
+            <div className="flex flex-col gap-2">
                 <h3 className='text-2xl mb-2 font-medium'>{item.title}</h3>
                 <span className='text-gray-600 mb-4 block'>
-                    <time className='bg-blue-800 text-white px-5 py-1.5 mr-4 rounded-xl'>
+                    <time className='bg-rose-800 text-white px-5 py-1.5 mr-4 rounded-xl'>
                         {item.year}
                     </time>
                     {item.category}
                 </span>
                 <p>{item.description}</p>
+                <h1 className="font-bold">Contributors</h1>
+                <div className="flex gap-2">
+                {item.contributors.map((contributor) => (
+                    <span className="font-medium">
+                        {contributor}
+                    </span>))}
+                </div>
             </div>
         </article>
     );
--- pages/projects.js
+++ pages/projects.js
@@ -10,16 +10,18 @@ const Projects = () => {
         {
             title: "Textbook Analysis (Alpha)",
             year: "2024*",
-            category: "Saas",
+            category: "Education",
             description: "Textbook Analysis is a GPT-4 powered course generator built on top of a full and feature rich LMS. It is built with NextJS, TailwindCSS, and Supabase. It uses the Supabase API to fetch courses and display them on the front end. It also uses the Supabase API to allow for users to create new courses and store them in the database. It also uses the GPT-4 API to generate custom course content on the fly based on any textbook or syllabus.",
             image: "/projects/textbookanalysis.png",
+            contributors: ["Sam"],
         },
         {
             title: "TCS Tracker Ultra",
             year: "2023",
-            category: "SaaS",
+            category: "Admin Dashboard",
             description: "TCS Tracker Ultra is a SaaS product that allows instructors at theCoderSchool to track their students' progress and attendance. It is built with the Pike13 API, NextJS, and TailwindCSS. Integrated recursive GPT-4 calls allow for instructors to generate custom reports and emails with the click of a button.",
             image: "/projects/tcstrackerultra.png",
+            contributors: ["Sam", "AJ"],
         },
         {
             title: "NBA Analytics",
@@ -27,13 +29,15 @@ const Projects = () => {
             category: "Data Science",
             description: "NBA Analytics is a data science project that uses Python, Pandas, and Flask for data collection and analysis. Data visualization is done with D3 and React to build custom charts and graphs. The project has GPT-4 integration for generating custom reports on the fly.",
             image: "/projects/nbaanalytics.png",
+            contributors: ["Ethan", "Sam", "Alexey"],
         },
         {
             title: "Byte Blogger",
             year: "2023",
-            category: "Database",
+            category: "Blog",
             description: "Byte Blogger is a personal dev blog built on the Supabase BaaS. It is built with NextJS, TailwindCSS, and Supabase. It uses the Supabase API to fetch blog posts and display them on the front end. It also uses the Supabase API to allow for users to create new blog posts and store them in the database.",
             image: "/projects/byteblogger.png",
+            contributors: ["Ethan", "Sam"],
         },
         {
             title: "Spelling Hive",
@@ -41,6 +45,7 @@ const Projects = () => {
             category: "Game",
             description: "Spelling Hive is a clone of the famous New York Times Spelling Bee game. It is built with Flask, React, and TailwindCSS. It uses the New York Times API to fetch the daily puzzle and display it on the front end. It also uses players to challenge each other and use an algorithm to show the best possible words to play.",
             image: "/projects/spellinghive.png",
+            contributors: ["Sam"],
         },
         {
             title: "3D Print Shop",
@@ -48,6 +53,7 @@ const Projects = () => {
             category: "E-Commerce",
             description: "3D Print Shop is an e-commerce store that sells 3D printed products. It is built with NextJS, TailwindCSS, and Stripe. It uses the Stripe API to process payments and the Stripe API to fetch products and display them on the front end. It also uses the Stripe API to allow for users to create new products and store them in the database.",
             image: "/projects/3dprintshop.png",
+            contributors: ["Sam"],
         },
     ]
 
@@ -77,7 +83,7 @@ const Projects = () => {
                 We like to build cool stuff. Whether you're looking for a custom website, a data science project, or a full stack application, we might know a thing or two. Check out some of our recent projects below to see what we've been up to. Let's get your project on the list next!
             </SectionTitle>
 
-            <section className='px-6 max-w-4xl mx-auto'>
+            <section className='px-6 max-w-6xl mx-auto'>
                 {projects.map((workItem) => (
                     <Work key={workItem.title} item={workItem} />
                 ))}
